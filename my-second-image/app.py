07168199a261efd647922e3fb47697f015553353
praneeth143a <praneeth@PraneethPC.localdomain>
2026-01-25 10:06:23 +0000

    In second image i had changed Dockerfile and app.py file

--- my-second-image/app.py
+++ my-second-image/app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Second image is working"
+    return "Flask_test is working"
 
-app.run(host="0.0.0.0", port=5005)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
