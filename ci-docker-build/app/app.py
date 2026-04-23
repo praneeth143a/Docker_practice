14cf301aba4f0f7d284232e72f94809301b09e19
Praneeth Kumar <praneethkumar4568@gmail.com>
2026-04-23 19:29:13 +0000

    removed secret and secured docker login

--- ci-docker-build/app/app.py
+++ ci-docker-build/app/app.py
@@ -1,1 +1,11 @@
-print("Hi, This is mini ci-test-pipeline")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hi, This is mini ci-test-pipeline 🚀"
+
+if __name__ == "__main__":
+    print("Server starting...")
+    app.run(host="0.0.0.0", port=5000)
--- ci-docker-build/build.sh
+++ ci-docker-build/build.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
-set -e   #to stop script on error
+set -e
 
-#we can use variables also for easy script
-
-#DOCKER_USERNAME="praneethkumar27"
-#DOCKER_IMAGE="ci-test-app"
-#IMAGE_TAG="latest"
+DOCKER_USERNAME="praneethkumar27"
+DOCKER_IMAGE="ci-test-app"
+IMAGE_TAG="latest"
 FULL_IMAGE_NAME="praneethkumar27/ci-test-app"
 
+CONTAINER_NAME="ci-test-container"
+APP_PORT=5000
 
-echo "build the docker image"
+echo "====================Build the docker image================="
 docker build -t ci-test-app ./app
 
-echo "tag the docker image"
+echo "=====================Tag the docker image====================="
 docker tag ci-test-app praneethkumar27/ci-test-app
 
-echo "login into docker"
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+echo "=====================Login into docker=================="
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-echo "push the image to docker hub"
+echo "==================Push the image to docker hub======================"
 docker push praneethkumar27/ci-test-app
 
-echo "image pushed successfully: $FULL_IMAGE_NAME"
+echo "=================image pushed successfully======================="
+
+echo "==================Remove old container if exists======================"
+if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+    docker stop $CONTAINER_NAME || true
+    docker rm $CONTAINER_NAME || true
+fi
+
+echo "==================Run the container======================"
+docker run -d -p $APP_PORT:$APP_PORT --name $CONTAINER_NAME praneethkumar27/ci-test-app
+
+echo "=================image pushed and running successfully======================="
+echo "APP: http://localhost:$APP_PORT"
