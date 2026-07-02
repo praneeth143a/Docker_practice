c580680bd4058c3e962fd8e6f3f6e0fa2ba0454a
Praneeth Kumar <praneethkumar4568@gmail.com>
2026-07-02 17:41:51 +0000

    Fix docker-web project structure

---
+++ docker-web/app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>Docker Web Project</h1>
+    <h2>Hello from Flask!</h2>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
